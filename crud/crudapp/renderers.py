9c8f1cdb11e719a12ec2f8afad98a9a4a995d647
Halit Celik <halit@cruncher.ch>
2019-05-14 17:34:37 +0200

    initial commit came at the end

---
+++ crud/crudapp/renderers.py
@@ -0,0 +1,12 @@
+import json
+from rest_framework.renderers import JSONRenderer
+
+
+class RecipeJSONRenderer(JSONRenderer):
+    charset = 'utf-8'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        print(data)
+        return json.dumps({
+            'recipes_object': data
+        })
